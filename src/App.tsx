6603b7dbe46dfa2aa9a5e9b039576fd04ec5002f
Den Great <gr8@nxt.ru>
2023-02-25 14:34:36 +0700

    auto-connect metamask

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useCallback, useEffect } from 'react';
+import { InjectedConnector } from "@web3-react/injected-connector";
+import { useWeb3React } from '@web3-react/core'
+import { usePersistentState } from 'react-persistent-state';
+
 import './App.css';
 
+const supportedChainIds = [1];
+
+const Injected = new InjectedConnector({
+  supportedChainIds,
+  // supportedChainIds: [42]
+});
+
+
 function App() {
+  const { activate, deactivate, active, chainId, account } = useWeb3React();
+  const [persistentAccount, setPersistentAccount] = usePersistentState('account');
+
+  // auto connect
+  useEffect(() => {
+    if(!active && persistentAccount) {
+      // todo: check persistent account address & chainId if they are changed then disconnect / don't try to connect
+      console.log('auto connect');
+      activate(Injected);
+    } else {
+      console.log('skip auto connect');
+    }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  // maintain persistent account
+  useEffect(() => {
+    if(account) {
+      console.log('set persist', account);
+      setPersistentAccount(account);
+    }
+  }, [account, setPersistentAccount]);
+
+  const disconnect = useCallback(() => {
+    console.log('disconnect');
+    setPersistentAccount('');
+    deactivate();
+  }, [deactivate, setPersistentAccount]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Metamask connect</h2>
+      <div>Supported chain: Ethereum</div>
+
+      { active ? (
+        <div>
+          <div>Account: {account}</div>
+          <div>Network ID: {chainId}</div>
+          <button onClick={disconnect}>Disconnect</button>
+        </div>
+      ) : (
+        <div>
+          <button onClick={() => { activate(Injected) }}>Connect</button>
+        </div>
+      ) }
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,21 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { Web3ReactProvider } from '@web3-react/core'
+import { Web3Provider } from "@ethersproject/providers";
+
+function getLibrary(provider: any) {
+  return new Web3Provider(provider);
+}
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Web3ReactProvider getLibrary={getLibrary}>
+      <App />
+    </Web3ReactProvider>
   </React.StrictMode>
 );
 
